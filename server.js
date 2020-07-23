2040ab2db8d060ab6865868e583ebb419278b65a
Dave Aitken <dave.aitken@gmail.com>
2020-07-23 15:44:56 +0100

    unzip bundle

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const morgan = require('morgan');
 const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const unzip = require('unzip');
+const stream = require('stream');
 
 const app = express();
 
@@ -9,9 +13,31 @@ app.use(morgan('dev'));
 app.use(fileUpload());
 app.use(express.static('static'));
 
+function unzipBundle(file, path, cb) {
+    let extract = unzip.Extract({ path: path });
+    extract.on('close', function () {
+    });
+
+    let readable = new stream.Readable();
+    readable._read = function () {};
+    readable.push(file.data);
+    readable.pipe(extract);
+    readable.push(null);
+}
+
 app.post('/upload', function (req, res) {
-    console.log(req.files);
-    res.send('ok')
+    if (!req.files || !req.files['bundle']) {
+        res.send('No file uploaded.');
+    } else {
+        let bundleDir = path.join(__dirname, 'static', 'bundle');
+        if (fs.existsSync(bundleDir)) {
+            fs.rmdirSync(bundleDir, { recursive: true });
+        }
+
+        unzipBundle(req.files['bundle'], bundleDir, function () {
+            res.send('ok');
+        });
+    }
 });
 
 app.get('/api/**', function (req, res) {
